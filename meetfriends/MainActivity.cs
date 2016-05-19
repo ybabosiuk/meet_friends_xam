7b6c2043453f7715c5c6ca9e3dc0a09fce1b344e
Yegor Babosiuk <babosiuk@Yegors-Mac-mini.local>
2016-05-19 08:45:39 +0300

    no message

---
+++ meetfriends/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace meetfriends
+{
+	[Activity (Label = "meetfriends", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+		}
+	}
+}
+
+
